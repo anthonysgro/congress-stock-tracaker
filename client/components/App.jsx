8805b78ae0defd9fb33155be5cd67a05221b973c
Anthony <AnthonySgro1995@gmail.com>
2021-12-26 15:03:41 -0500

    first commit

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return "Welcome to Tamanna's Project!";
+    }
+}
+
+export default App;
