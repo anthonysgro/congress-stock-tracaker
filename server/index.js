88b97789138d1df7cd6f3e02f09a00d210e40b6d
Anthony <AnthonySgro1995@gmail.com>
2021-12-26 20:14:03 -0500

    Made Morgan a regular dependency

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.use(morgan("dev"));
 
 // Api Routes
-app.use("/api/nancy", nancy);
+app.use("/api", nancy);
 
 // Send the app
 app.get("/", (req, res) => {
